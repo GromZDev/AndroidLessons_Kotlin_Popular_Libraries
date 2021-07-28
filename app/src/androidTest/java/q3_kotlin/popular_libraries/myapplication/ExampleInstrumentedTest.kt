533bbf9aec3da49376847bddbb7d54c54346e17b
GromZDev <Dmitryev@mail.ru>
2021-07-29 00:39:15 +0300

    Создание пустого проекта.

---
+++ app/src/androidTest/java/q3_kotlin/popular_libraries/myapplication/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package q3_kotlin.popular_libraries.myapplication
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("q3_kotlin.popular_libraries.myapplication", appContext.packageName)
+    }
+}
